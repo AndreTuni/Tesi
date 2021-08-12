f0b4741f004796b64f640976a8fd09f68f85e6ba
AndreTuni <tuni99.at@gmail.com>
2021-08-12 10:02:29 +0200

    fix conversione

--- DicomToPng/Test/converter.py
+++ DicomToPng/Test/converter.py
@@ -1,21 +1,21 @@
 import numpy as np
-#from image_utils import imgtonumpy, numpytoimg, normalization, shape_as
+from image_utils import imgtonumpy, numpytoimg, normalization, shape_as
 import pydicom
 from pydicom.pixel_data_handlers.util import apply_voi_lut
 from pydicom.pixel_data_handlers.util import apply_modality_lut
 from PIL import Image
 
-ds = pydicom.dcmread('1.dicom')
+ds = pydicom.dcmread('4.dicom')
 
 
 new_image = ds.pixel_array.astype(float)
 
 lut = pydicom.pixel_data_handlers.util.apply_modality_lut(new_image, ds)
 lut = pydicom.pixel_data_handlers.util.apply_voi_lut(lut, ds)
-#lut = normalization(lut)
+lut = normalization(lut)
 if ds[0x0028,0x0004].value == "MONOCHROME1":
 	lut = -1*(lut-255)
-#lut = shape_as(lut)
+lut = shape_as(lut)
 
 scaled_image = (np.maximum(lut, 0) / lut.max()) * 255.0
 
@@ -23,6 +23,6 @@ scaled_image = np.uint8(scaled_image)
 
 final_image = Image.fromarray(scaled_image)
 
-final_image.save('image1.png')
+final_image.save('image4.png')
 
 
