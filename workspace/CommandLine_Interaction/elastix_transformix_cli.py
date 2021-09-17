c3ff3978bb148fdc4388d560c280a4906986df2f
AndreTuni <tuni99.at@gmail.com>
2021-09-17 12:33:05 +0200

    fresh start

---
+++ workspace/CommandLine_Interaction/elastix_transformix_cli.py
@@ -0,0 +1,5 @@
+import os
+import subprocess
+subprocess.call([r'./Scripts/registration.bat'])
+#os.system('cmd /k "cd C:\\Users\\tunit\\Documents\\GitHub\\CXR_preprocessing\\CommandLine_Interaction"')
+#os.system('cmd /k'"elastix --help")
